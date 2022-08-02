3c4cffd51607d39e23e7ee9209f006f66300435a
Mariana Greco Mantovani <94694482+grecosz@users.noreply.github.com>
2022-08-02 11:31:03 -0300

    Base

---
+++ Diagrama de Classe/Diagrama de Classe/Pessoa.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Diagrama_de_Classe
+{
+    internal class Pessoa
+    {
+        // Atributo de Classe
+        private string nome;
+        private int idade = 0;
+        private double= altura;
+        private string sexo;
+
+        //Construtor sem parametros
+        public Pessoa()
+        {
+         this.nome = null;
+         this.idade = 0;
+         this.altura = 0.0;
+         Sexo = null;
+
+
+        }
+        //Construtor com parametros
+        public Pessoa (string nome, int idade, double altura, char sexo)
+        {
+            this.nome = nome;
+            this.idade = idade;
+            this.altura = altura;
+            Sexo = sexo.ToString();
+
+  
+        }
+        //Propiedade do campo nome para acesso ao seu conteudo
+        public string Nome
+        { get { return nome; }  //retorna o valor armazenado em um atributo
+          set { nome = value; } //armazena o valor no campo
+        }
+         //
+
+        //Método que retorna uma string contendo as informações da pessoa
+        public override string ToString()
+        {
+            return(String.Format("Nome: {0}", nome));
+        }
+    }
+}
---
+++ Diagrama de Classe/Diagrama de Classe/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Diagrama_de_Classe
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Criação de um objeto p, do tipo da classe Pessoa
+            Pessoa p = new Pessoa();
+
+            // Exibindo na tela informações do objeto p
+            // Realizando uma chamada no método ToString
+            Console.WriteLine(p.ToString());
+
+            // Atribuindo um nome ao objeto p, por meio da propriedade nome
+            p.Nome = "Nivia";
+
+            //Exibindo na tela informações do objeto p,
+            // após a atribuição do nome
+            Console.WriteLine(p);
+            Console.ReadKey();
+
+        }
+    }
+}
